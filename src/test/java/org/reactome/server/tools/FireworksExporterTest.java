1fc2487b38d0aaf6b29c16522bede0b406c355fb
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-11-27 16:36:48 +0000

    scaffolding

---
+++ src/test/java/org/reactome/server/tools/FireworksExporterTest.java
@@ -0,0 +1,38 @@
+package org.reactome.server.tools;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple FireworksExporter.
+ */
+public class FireworksExporterTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public FireworksExporterTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( FireworksExporterTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
